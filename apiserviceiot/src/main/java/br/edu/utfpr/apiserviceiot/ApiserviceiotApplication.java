2ebf27f0a819a420d7424fc211ba60d077d08bc3
Carlos Moyses <54774598+carlosmoyses-ai@users.noreply.github.com>
2024-04-11 17:36:03 -0300

    apiserviceiot base inicial

---
+++ apiserviceiot/src/main/java/br/edu/utfpr/apiserviceiot/ApiserviceiotApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.apiserviceiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiserviceiotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiserviceiotApplication.class, args);
+	}
+
+}
---
+++ apiserviceiot/src/test/java/br/edu/utfpr/apiserviceiot/ApiserviceiotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.apiserviceiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiserviceiotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
