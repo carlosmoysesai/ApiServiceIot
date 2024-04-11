f64eb53f984e7cc72910f58682109a3dfe4fbf62
Carlos Moyses <54774598+carlosmoyses-ai@users.noreply.github.com>
2024-04-11 18:02:01 -0300

    Criando a estrutura das Pastas

--- apiserviceiot/src/main/java/br/edu/utfpr/apiserviceiot/Application.java
+++ apiserviceiot/src/main/java/br/edu/utfpr/apiserviceiot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiserviceiotApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiserviceiotApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
+ 
 }
