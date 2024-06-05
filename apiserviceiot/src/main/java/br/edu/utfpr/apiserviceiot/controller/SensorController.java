1e566a37727e27c73b6111d07e9aa1b3b2507d9c
Carlos Moyses <54774598+carlosmoyses-ai@users.noreply.github.com>
2024-06-05 16:58:41 -0300

    configuração do swagger

--- apiserviceiot/src/main/java/br/edu/utfpr/apiserviceiot/controller/SensorController.java
+++ apiserviceiot/src/main/java/br/edu/utfpr/apiserviceiot/controller/SensorController.java
@@ -18,9 +18,11 @@ import br.edu.utfpr.apiserviceiot.dto.SensorDTO;
 import br.edu.utfpr.apiserviceiot.exception.NotFoundException;
 import br.edu.utfpr.apiserviceiot.model.Sensor;
 import br.edu.utfpr.apiserviceiot.service.SensorService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/sensors")
+@Tag(name = "Sensores", description = "Registro de Sensores")
 public class SensorController {
 
     @Autowired
@@ -31,7 +33,7 @@ public class SensorController {
         try {
             var res = sensorService.create(dto);
             return ResponseEntity.status(HttpStatus.CREATED).body(res);
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
         }
     }
@@ -44,19 +46,19 @@ public class SensorController {
     @GetMapping("/{id}")
     public ResponseEntity<Object> getById(@PathVariable long id) {
         var gateway = sensorService.getById(id);
-        
+
         return gateway.isPresent()
-            ? ResponseEntity.ok().body(gateway.get())
-            : ResponseEntity.notFound().build();
+                ? ResponseEntity.ok().body(gateway.get())
+                : ResponseEntity.notFound().build();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> update(@PathVariable long id, @RequestBody SensorDTO dto) {
         try {
             return ResponseEntity.ok().body(sensorService.update(id, dto));
-        } catch(NotFoundException ex) {
+        } catch (NotFoundException ex) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
         }
     }
@@ -66,9 +68,9 @@ public class SensorController {
         try {
             sensorService.delete(id);
             return ResponseEntity.ok().build();
-        } catch(NotFoundException ex) {
+        } catch (NotFoundException ex) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
         }
     }
